a20291cf55ad0da32269af29368857c894d0cab5
kayros-paradox <vugi915@gmail.com>
2025-08-08 21:37:14 +0500

    update repository files

---
+++ update-all.sh
@@ -0,0 +1,5 @@
+#!/bin/bash/
+
+git add .
+git commit -m "update repository files"
+git push origin
---
+++ update-readme.sh
@@ -0,0 +1,5 @@
+#!/bin/bash/
+
+git add README.md README.ru.md
+git commit -m "update README.md"
+git push origin
